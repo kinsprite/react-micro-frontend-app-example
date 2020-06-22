f93c3b35dcc8eaaeaba11ec6aa01fb472b01c676
kinsprite <qxqs11@126.com>
2020-06-22 14:29:27 +0800

    use registerApp()

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // import React from 'react';
-import { register } from 'react-micro-frontend-framework';
+import { registerApp } from 'react-micro-frontend-framework';
 
 import SubApp from './SubApp';
 
-register.registerComponent('app-example', SubApp);
+registerApp('app-example', {
+  component: SubApp,
+});
