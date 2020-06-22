3a447e253dbc7145b787506b4e0b69dc02382b90
kinsprite <qxqs11@126.com>
2020-06-22 18:40:26 +0800

    update dependencies

--- public/api/metadata/apps.js
+++ public/api/metadata/apps.js
@@ -7,8 +7,9 @@
       id: 'app-example',
       dependencies: ['echart', 'app1'], // NOT implement yet
       entries: [
-        '/rmf-app-example/658c5f91/app-example-a200b003.css',
-        '/rmf-app-example/658c5f91/app-example-b889c22.js'
+        // REMOVE the current app's entries in development
+        // '/rmf-app-example/658c5f91/app-example-a200b003.css',
+        // '/rmf-app-example/658c5f91/app-example-b889c22.js'
       ],
       routes: ['/app-example'],
     },
